58c3454391a020f4f3a4a8c0db01c9df237d4e49
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2023-02-22 11:01:42 -0500

    initial commit

---
+++ brain/perfume_brain.py
@@ -0,0 +1,88 @@
+import json
+import pandas as pd
+from collections import OrderedDict
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
+class PerfumeBrain:
+
+    # This is the brain of our product. It can predict a perfume based on accords or notes provided.
+    @staticmethod
+    def get_recommendation(input_data):
+        # Load the perfumes.json file
+        with open('../json/perfumes.json', 'r') as f:
+            data = json.load(f)
+
+        # Extract the perfume names and their notes and accords
+        perfumes = []
+        for perfume in data['perfumes']:
+            name = perfume['name']
+            try:
+                notes = ' '.join(
+                    [note['name'].lower() for note in perfume['top_notes'] + perfume['middle_notes'] + perfume['base_notes']])
+            except:
+                continue
+            accords = ' '.join([accord['name'].lower() for accord in perfume['accords']])
+            perfumes.append((name, notes + ' ' + accords))
+
+        # Convert the data to a Pandas dataframe
+        df = pd.DataFrame(perfumes, columns=['name', 'text'])
+
+        # Preprocess the input
+        input_text = ' '.join(input_data['top_notes'] + input_data['middle_notes'] + input_data['base_notes'])
+        input_text += ' ' + ' '.join([accord.lower() for accord in input_data['accords']])
+
+        # Train a machine learning model
+        vectorizer = CountVectorizer()
+        x_train = vectorizer.fit_transform(df['text'])
+        y_train = df['name']
+        clf = MultinomialNB()
+        clf.fit(x_train, y_train)
+
+        # Use the model to predict the fragrance name
+        x_input = vectorizer.transform([input_text])
+        y_pred = clf.predict(x_input)
+        # {
+        #     "accords": ["powdery", "vanilla", "floral", "citrus", "woody", "cinnamon"],
+        #     "top_notes": ["bergamot", "cinnamon"],
+        #     "middle_notes": ["vanilla"],
+        #     "base_notes": ["leather"]
+        # }
+
+        for perfume in data['perfumes']:
+            if perfume['name'] == y_pred[0]:
+                return perfume
+        else:
+            return {}
+
+    @staticmethod
+    def get_all_perfumes():
+        with open('../json/perfumes.json') as f:
+            return json.load(f)
+
+    @staticmethod
+    def get_all_notes():
+        with open('../json/notes.json') as f:
+            return json.load(f)
+
+    @staticmethod
+    def get_notes_by_category(note_type):
+        with open('../json/notes.json') as f:
+            notes = json.load(f)
+            for note in notes['perfume_notes']:
+                if note['name'] == note_type:
+                    return note
+            else:
+                return {}
+
+    @staticmethod
+    def get_notes_categories():
+        with open('../json/notes.json') as f:
+            notes = json.load(f)
+
+            categories = []
+            for category in notes['perfume_notes']:
+                categories.append(category['name'])
+
+            return categories
---
+++ routes/route.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+
+from brain.perfume_brain import PerfumeBrain
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.route('/perfumes')
+def perfumes():
+    return PerfumeBrain().get_all_perfumes()
+
+
+@app.route('/perfumes/recommendation', methods=['POST'])
+def recommendation():
+    return PerfumeBrain().get_recommendation(request.get_json(force=True))
+
+
+@app.route('/notes')
+def notes():
+    return PerfumeBrain().get_all_notes()
+
+
+@app.route('/notes/categories')
+def notes_categories():
+    return PerfumeBrain().get_notes_categories()
+
+
+@app.route('/notes/<category>')
+def notes_by_type(category):
+    return PerfumeBrain().get_notes_by_category(category)
