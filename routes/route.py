d959e1457a63fb9b0a3f63e4be06c5c922194c23
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2023-02-22 11:49:28 -0500

    go live dependencies

--- routes/route.py
+++ routes/route.py
@@ -1,13 +1,10 @@
-import os
 from flask import Flask, request
+import os
 
 from brain.perfume_brain import PerfumeBrain
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
